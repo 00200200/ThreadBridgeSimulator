edf1c722c0941ff7e21451348dbdf753db926ad5
Michał Furgała <83299832+00200200@users.noreply.github.com>
2023-11-27 23:04:58 +0100

    OneCarFewLabels Works

--- src/main/java/pl/SimulateBridge/simulation/Bridge.java
+++ src/main/java/pl/SimulateBridge/simulation/Bridge.java
@@ -10,15 +10,17 @@ public class Bridge {
     private int carsOnBridge;
     public List<Label> bridgeLabels;
     public List<Label> queueLabels;
+    public List<Label> allLabels;
     public int currentCapacity;
 
 
-    public Bridge(int maxCapacity, int length, List<Label> bridgeLabels, List<Label> queueLabels){
+    public Bridge(int maxCapacity, int length, List<Label> bridgeLabels, List<Label> queueLabels,List<Label> allLabels){
         this.maxCapacity = maxCapacity;
         this.length = length;
         this.bridgeLabels = bridgeLabels;
         this.queueLabels = queueLabels;
         this.currentCapacity = 0;
+        this.allLabels = allLabels;
     };
 
     public synchronized boolean enter(Car car){
